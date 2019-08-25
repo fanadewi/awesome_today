785d49f9c3f9839a8744bb9f0ea5040b0813c345
Fitri Ana Dewi <fitriholly@gmail.com>
2019-08-25 20:22:20 +0700

    enabled turbolinks and caching data

--- app/controllers/births_controller.rb
+++ app/controllers/births_controller.rb
@@ -1,6 +1,8 @@
 class BirthsController < ApplicationController
   def index
-    data = GetToday.new.to_attributes
+    data = Rails.cache.fetch("births_#{Date.today}", expires_in: Time.now.end_of_day - Time.now) do
+      GetToday.new.to_attributes
+    end
     @today = Kaminari.paginate_array(data[:births]).page(params[:page])
   end
 end
--- app/controllers/deaths_controller.rb
+++ app/controllers/deaths_controller.rb
@@ -1,6 +1,8 @@
 class DeathsController < ApplicationController
   def index
-    data = GetToday.new.to_attributes
+    data = Rails.cache.fetch("deaths_#{Date.today}", expires_in: Time.now.end_of_day - Time.now) do
+      GetToday.new.to_attributes
+    end
     @today = Kaminari.paginate_array(data[:deaths]).page(params[:page])
   end
 end
